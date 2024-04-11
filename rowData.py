f0ac15ffbe67d7cfd4c24edd451d68597f0df25a
Monoget Saha <monoget2@gmail.com>
2024-04-12 01:23:35 +0600

    update data scrapper

--- rowData.py
+++ rowData.py
@@ -5,7 +5,7 @@ from openpyxl.styles import Alignment
 from openpyxl.utils import get_column_letter
 
 # Folder containing Excel files
-folder_path = 'canada/2.british-columbia/vancouver/'
+folder_path = 'canada/2.british-columbia/victoria/'
 output_file = folder_path+'combined_excel.xlsx'
 
 # Columns to collect from each Excel file
